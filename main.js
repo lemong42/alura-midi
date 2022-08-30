caefb5c3b62ab2abc76a00ed77521075bdaa2297
lemong42 <danellers@gmail.com>
2022-08-29 21:05:40 -0300

    version 1.0

---
+++ main.js
@@ -0,0 +1,33 @@
+const listaTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+for (i = 0; i < listaTeclas.length; i++) {
+    const tecla = listaTeclas[i];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+    }
+}
+
